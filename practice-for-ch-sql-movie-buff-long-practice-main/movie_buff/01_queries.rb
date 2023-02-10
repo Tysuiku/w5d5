2bf9cc5f5d86c3146490d08f6ae1589ba677c8fb
Cory Boris <tromboris@gmail.com>
2023-02-10 16:04:54 -0500

    finish most of harrison ford

--- practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
+++ practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
@@ -20,6 +20,11 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford appeared but not
   # as a lead actor.
+  Actor
+    .joins(:movies)
+    .where(actors: {name: "Harrison Ford"})
+    .where.not(castings: {ord: 1})
+    .select("movie_id", "title")
 
 end
 
